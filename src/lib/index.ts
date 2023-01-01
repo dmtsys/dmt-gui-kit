e8fde94f12df9ab7f8da957716d60abde6c4901a
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-01 13:55:04 +0000

    update index.ts exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
-export { default as LogView } from './components/LogView.svelte'
-export { default as Colors } from './components/Colors.svelte'
-export { default as Loading } from './components/Loading.svelte'
-export { default as GuiErrors } from './components/GuiErrors.svelte'
+export { default as LogView } from '$lib/components/LogView.svelte';
+export { default as Colors } from '$lib/components/Colors.svelte';
+export { default as Loading } from '$lib/components/Loading.svelte';
+export { default as GuiErrors } from '$lib/components/GuiErrors.svelte';
+export { default as SnackBar } from '$lib/components/SnackBar.svelte';
 
-export { default as logStore } from './store/logStore'
+export { default as logStore } from '$lib/store/logStore';
+export { snackbar } from '$lib/store/snack';
