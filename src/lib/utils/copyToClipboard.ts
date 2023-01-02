8fade10e124140473503a132f449621e4e6b6a00
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-02 23:39:46 +0000

    add copyToClipboard util

---
+++ src/lib/utils/copyToClipboard.ts
@@ -0,0 +1,46 @@
+import { snackbar } from '$lib/store/snack';
+
+/**
+ *
+ * @param text Text to copy.
+ * @param successMessage message to be displayed on snackbar, when it is successful. Defaults to "Text copied to clipboard."
+ * @returns `true` if copy is successful else `false`.
+ *
+ */
+export const copyToClipboard = async (
+  text: string,
+  successMessage?: string
+) => {
+  const message = successMessage || 'Text copied to clipboard.';
+  let textToCopy = text;
+
+  if (!textToCopy) {
+    snackbar.show('Nothing to copy.',{color:'danger',timeout: 1500});
+    return false;
+  }
+
+  if (navigator.clipboard) {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      snackbar.show(message);
+      return true;
+    } catch (error) {
+      // let's try the fallback below
+    }
+  }
+
+  // fallback
+  try {
+    const input = document.createElement('textarea');
+    input.innerHTML = textToCopy;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    snackbar.show(message);
+    return true;
+  } catch (error) {
+    snackbar.show('Could not copy to clipboard.',{color:'danger',timeout: 1500});
+    return false;
+  }
+};
