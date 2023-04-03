60a5a520f66d8cdb9610e221d31ef3305149739c
David Krmpotic <david.krmpotic@gmail.com>
2023-04-03 17:57:29 +0200

    add Noise component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ export { default as LogView } from '$lib/components/LogView.svelte';
 export { default as Loading } from '$lib/components/Loading.svelte';
 export { default as GuiErrors } from '$lib/components/GuiErrors.svelte';
 export { default as SnackBar } from '$lib/components/SnackBar.svelte';
+export { default as Noise } from '$lib/components/Noise.svelte';
 
 export { default as logStore } from '$lib/store/logStore';
 export * from '$lib/store/snack';
